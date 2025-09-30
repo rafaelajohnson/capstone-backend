c3ac8600a27ff024cfe7d63416f7134307816961
Rafaela Johnson <rafaelajohnson1@gmail.com>
2025-09-29 21:59:43 -0400

    added comment to code

--- middleware/handlePostgresErrors.js
+++ middleware/handlePostgresErrors.js
@@ -1,18 +1,20 @@
+// middleware/handlePostgresErrors.js
 const ERRORS = {
   INVALID_TYPE: "22P02",
   UNIQUE_VIOLATION: "23505",
   FOREIGN_KEY_VIOLATION: "23503",
 };
 
-/** Uses PostgreSQL error codes to send slightly better error messages */
+// Uses PostgreSQL error codes to send cleaner error messages to clients
 export default function handlePostgresErrors(err, req, res, next) {
   switch (err.code) {
     case ERRORS.INVALID_TYPE:
-      return res.status(400).send(err.message);
-    case ERRORS.FOREIGN_KEY_VIOLATION:
+      return res.status(400).json({ error: err.message });
     case ERRORS.UNIQUE_VIOLATION:
-      return res.status(400).send(err.detail);
+    case ERRORS.FOREIGN_KEY_VIOLATION:
+      return res.status(400).json({ error: err.detail });
     default:
-      next(err);
+      // Let any unknown error bubble up
+      return next(err);
   }
 }
