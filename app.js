408c929da9c15b36c55e8fc91c26560ec19ef039
Rafaela Johnson <rafaelajohnson1@gmail.com>
2025-09-29 22:31:59 -0400

    wrapped middleware in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import usersRoutes from "./api/users.js";
 import storiesRoutes from "./api/stories.js";
 import pagesRoutes from "./api/pages.js";
 import optionsRoutes from "./api/options.js";
+import getUserFromToken from "./middleware/getUserFromToken.js";
 import handlePostgresErrors from "./middleware/handlePostgresErrors.js";
 
 const app = express();
@@ -10,8 +11,11 @@ const app = express();
 // parse JSON bodies
 app.use(express.json());
 
+// attach user from token if provided
+app.use(getUserFromToken);
+
 // mount routes
-app.use("/auth", usersRoutes);     // signup + login
+app.use("/auth", usersRoutes);      // signup + login
 app.use("/stories", storiesRoutes); // save + get stories
 app.use("/pages", pagesRoutes);     // get page + options
 app.use("/options", optionsRoutes); // add/fetch options
@@ -21,6 +25,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use(handlePostgresErrors); // add the error handling middleware
+// centralized error handling (middleware)
+app.use(handlePostgresErrors);
 
 export default app;
